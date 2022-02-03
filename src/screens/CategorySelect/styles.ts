926867a59d28a4280e7e0985142594f55fccdbc2
Roger Vargas <rogerfsvargas@hotmail.com>
2022-02-03 16:03:19 -0300

    resolvendo bug de modal nao abrir no android

--- src/screens/CategorySelect/styles.ts
+++ src/screens/CategorySelect/styles.ts
@@ -2,11 +2,13 @@ import styled from 'styled-components/native';
 import { RFValue } from 'react-native-responsive-fontsize';
 import { Feather } from '@expo/vector-icons';
 
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
 interface CategoryProps {
   isActive: boolean;
 }
 
-export const Container = styled.View`
+export const Container = styled(GestureHandlerRootView)`
   flex: 1;
   background-color: ${({ theme }) => theme.colors.background};
 `;
@@ -37,7 +39,8 @@ export const Category = styled.TouchableOpacity<CategoryProps>`
   flex-direction: row;
   align-items: center;
 
-  background-color: ${({ isActive, theme }) => isActive ? theme.colors.secondary_light : theme.colors.background};
+  background-color: ${({ isActive, theme }) =>
+    isActive ? theme.colors.secondary_light : theme.colors.background};
 `;
 
 export const Icon = styled(Feather)`
